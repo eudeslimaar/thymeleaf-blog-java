f8da7b734f649d2eff1c8c81dd52d6271abd7b67
Eudes Lima <eudesfilho1994@outlook.com>
2024-10-05 17:15:40 -0300

    create SecurityConfig

---
+++ src/main/java/com/eudeslima/blogbackend/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.eudeslima.blogbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/posts/**", "/admin/**", "/css/**").permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "/api/posts/").permitAll();
+                    auth.anyRequest().authenticated();
+                });
+
+        return httpSecurity.build();
+    }
+}
